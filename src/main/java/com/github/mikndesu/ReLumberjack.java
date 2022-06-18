709e20a33ad6523cd9110cbfa1a1fb58b020ac15
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-06-19 02:06:27 +0900

    setting up environment

---
+++ src/main/java/com/github/mikndesu/ReLumberjack.java
@@ -0,0 +1,11 @@
+package com.github.mikndesu;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ReLumberjack implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+    
+}
