8fc2326dcf99f2be0415c6fd2d4ea898e11d9443
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-06-22 14:01:04 +0900

    add comment on Mod Menu Screen

--- src/main/java/com/github/mikndesu/re_lumberjack/config/ReLumberjackConfig.java
+++ src/main/java/com/github/mikndesu/re_lumberjack/config/ReLumberjackConfig.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.github.mikndesu.re_lumberjack.ReLumberjack;
-
+import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.RequiresRestart;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = ReLumberjack.MODID)
 public class ReLumberjackConfig implements ConfigData {
-    private final List<String> registryNameList = new ArrayList<>();
+    @RequiresRestart
+    @Comment("Changes will be applied to your client after restarting.")
+    private List<String> registryNameList = new ArrayList<>();
 }
