ce47b460f3a692a8c30152b599e444e72611e5c1
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-06-22 13:19:24 +0900

    add registry name list

--- src/main/java/com/github/mikndesu/re_lumberjack/ReLumberjack.java
+++ src/main/java/com/github/mikndesu/re_lumberjack/ReLumberjack.java
@@ -1,17 +1,27 @@
 package com.github.mikndesu.re_lumberjack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.github.mikndesu.re_lumberjack.config.ReLumberjackConfig;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class ReLumberjack implements ModInitializer {
 
     public static final Logger LOGGER = LogManager.getLogger("ReLumberJack/Main");
     public static final String MODID = "re_lumberjack";
+    public static List<BlockState> acceptedBlocks = new ArrayList<>();
 
     @Override
     public void onInitialize() {
+        AutoConfig.register(ReLumberjackConfig.class, JanksonConfigSerializer::new);
     }
     
 }
--- src/main/java/com/github/mikndesu/re_lumberjack/config/ReLumberjackConfig.java
+++ src/main/java/com/github/mikndesu/re_lumberjack/config/ReLumberjackConfig.java
@@ -1,5 +1,8 @@
 package com.github.mikndesu.re_lumberjack.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.github.mikndesu.re_lumberjack.ReLumberjack;
 
 import me.shedaniel.autoconfig.ConfigData;
@@ -7,4 +10,5 @@ import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = ReLumberjack.MODID)
 public class ReLumberjackConfig implements ConfigData {
+    private final List<String> registryNameList = new ArrayList<>();
 }
